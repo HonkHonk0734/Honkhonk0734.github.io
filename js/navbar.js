3fbd43f9412584e9f6f236c365aa6a11267a0055
HonkHonk0734 <tuinhoutedric@gmail.com>
2021-12-17 23:17:28 +0100

    update the navbar js and css + the html

--- js/navbar.js
+++ js/navbar.js
@@ -1,26 +1,10 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-  
-    burger.addEventListener('click', () => {
-      //Toggle Nav
-      nav.classList.toggle('nav-active');
-      //Animate Links
-      navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-          link.style.animation = '';
-        } else {
-          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 6 + 0.5}s`;
-        }
-  
-      });
-      //burger animatie
-      burger.classList.toggle('toggle');
-  
-  
-    });
-  
-  }
-  
-  navSlide();
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('open')
+  links.forEach(link =>{
+    link.classList.toggle("fade")
+  });
+});
